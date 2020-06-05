0218add4f4cd36aa751d76821307362ead2c1afb
Angel Klyce <aklyce@berkeley.edu>
2020-06-05 14:17:37 -0700

    Updates

---
+++ precache-manifest.2304aaf5194702c3373ee49671ac498c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9a3e39a84817b7823a453f8a4405e0",
+    "url": "/aklyce.github.io/index.html"
+  },
+  {
+    "revision": "d7e7ae55f7c4138e9ed9",
+    "url": "/aklyce.github.io/static/css/main.e51bca42.chunk.css"
+  },
+  {
+    "revision": "0ea7db59485acfb106f8",
+    "url": "/aklyce.github.io/static/js/2.8296cd0e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/aklyce.github.io/static/js/2.8296cd0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e7ae55f7c4138e9ed9",
+    "url": "/aklyce.github.io/static/js/main.0ad9aca6.chunk.js"
+  },
+  {
+    "revision": "dec9d6e0666615fdb87f",
+    "url": "/aklyce.github.io/static/js/runtime-main.02125791.js"
+  },
+  {
+    "revision": "585da74111c45968689f7a13b93a4ff2",
+    "url": "/aklyce.github.io/static/media/IMG_6565.585da741.jpg"
+  }
+]);
